b87a478aa42f4f0ce01b6c761a131d070d2ce2fe
Prodip <prodip@tecognize.com>
2022-12-14 15:32:14 +0600

    Now the folder name collection program is writting all the child directories except first child

--- folder_name_collection.py
+++ folder_name_collection.py
@@ -5,65 +5,27 @@ dir_path = input('Enter the full path of the directory: ')
 path_list = dir_path.split('/')
 dir_name = path_list.pop()
 
-
-list_of_subdir = os.listdir(dir_path)
-
-# subdirs = [x[1] for x in os.walk(dir_path)]
-
-# new_list_of_subdir = []
-
-# for item in list_of_subdir:
-#     if os.path.isdir(dir_path+"/"+item):
-#         if(item[0] != '.'):
-#             new_list_of_subdir.append(item)
-
-
-# with open('./sub_directory_names.txt', 'w') as output_file:
-#     for item in new_list_of_subdir:
-#         output_file.write("%s\n" %item)
-
-#     print(new_list_of_subdir)
-
-#     print('All the name of the sub directories in "%s" are now printed on sub_directory_names.txt file' %dir_name)
-
-# subdirs = os.walk(dir_name)
-
-# for item in subdirs:
-#     print(item)
-
-# print(subdirs[1])
-
-# print(subdirs)
-
-total_list = []
-
+total_subdir_list = []
 for (path, dirs, files) in os.walk(dir_path):
-    # print(path)
     if dirs:
-        total_list.append(dirs)
-        # print(dirs)
-        # print("--------")
-    # print(files)
+        total_subdir_list.append(dirs)
+
+total_subdir_list.pop(0)
+
+subdir_list_without_first_child = []
+for subdirs in total_subdir_list:
+    for subdir in subdirs:
+        if subdir.__contains__('('):
+            for index, char in enumerate(subdir):
+                if char == "(":
+                    name_without_braces = (subdir[:index-1])
+                    subdir_list_without_first_child.append(name_without_braces)
+        else:
+            subdir_list_without_first_child.append(subdir)
 
 
-total_list.pop(0)
+with open('./sub_directory_names.txt', 'w') as output_file:
+    for item in subdir_list_without_first_child:
+        output_file.write("%s\n" %item)
 
-# print(total_list)
-
-new_total_list = []
-
-for item in total_list:
-    for i in item:
-        new_total_list.append(i)
-
-
-for index, char in enumerate(new_total_list[1]):
-    if char == "(":
-        new_name = (new_total_list[1])[:index]
-        print(new_name)
-
-# print(new_total_list)
-
-# with open('./sub_directory_names.txt', 'w') as output_file:
-#     for item in new_total_list:
-#         output_file.write("%s\n" %item)
+# print(subdir_list_without_first_child)
